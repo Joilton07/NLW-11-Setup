8de7e4aead327bea25d4b5f9dc1cbb88f333a8b8
Joilton07 <joilton@aluno.fapce.edu.br>
2023-01-26 11:50:46 -0300

    update server.ts Host

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ async function server() {
     app.register(cors)
     app.register(appRoutes)
 
-    await app.listen({ port: Number(process.env.PORT) || 3333 }).then(() => {
+    await app.listen({ port: Number(process.env.PORT) || 3333, host: process.env.HOST || '0.0.0.0' }).then(() => {
         console.log('HTTP Server Running ', process.env.PORT)
     })
 }
