738cdf524e212dfa3facee2556df8e5141dd9966
Joilton07 <joilton@aluno.fapce.edu.br>
2023-01-26 10:47:37 -0300

    update package.json part 6

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,15 @@ import cors from '@fastify/cors'
 import Fastify from 'fastify'
 import { appRoutes } from './routes'
 
-const app = Fastify()
+async function server() {
+    const app = Fastify()
 
-app.register(cors)
-app.register(appRoutes)
+    app.register(cors)
+    app.register(appRoutes)
 
-app.listen({
-    port: 3333
-}).then(() => {
-    console.log('HTTP Server Running')
-})
+    await app.listen({ port: Number(process.env.PORT) || 3333 }).then(() => {
+        console.log('HTTP Server Running')
+    })
+}
+
+server()
